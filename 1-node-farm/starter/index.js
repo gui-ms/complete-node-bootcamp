3c2f714e059af53c3698a3b46a19e423be584249
Guilherme Martins <gui.martins.soares@gmail.com>
2025-02-01 16:25:52 -0300

    Added example of file manipulation using async functions

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,24 @@
 const fs = require('fs')
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
+//Synchronous way - blocking
+/* const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
 console.log(textInput);
 
 const textOut = `Lorem Ipsum ipsis literis: ${textInput} \nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 
 console.log("File written!");
+ */
+//Asynchronous way - non-blocking
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log("Reading from file " + data);
+    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data1) => {
+        console.log(data1);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data2) => {
+            console.log(data2);
+            fs.writeFile(`./txt/final.txt`, `${data1} \n ${data2}`, 'utf-8', err => {
+                console.log("File written!");
+            } )
+        })
+    })
+} )
