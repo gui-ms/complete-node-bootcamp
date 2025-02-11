a9f354c083923e85fae1a19a8548a43bf02a6a33
Guilherme Martins <gui.martins.soares@gmail.com>
2025-02-11 15:19:16 -0300

    fixed template files and finished server side configuration

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const { json } = require('stream/consumers');
 const url = require('url');
 
 //*****************************************************
@@ -33,13 +34,50 @@ console.log("File written!");
 
 //******************************************* 
 // SERVER
-const server = http.createServer((req, res) =>{
-    const pathName = req.url;
-    if (pathName === '/' || pathName === '/overview') {
-    res.end("This is overview!")
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if(!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
     }
-    else if (pathName === '/product') {
-        res.end("This is the Product page!")
+    return output;
+}
+
+//Top level code which is executed only once, hence we can use the synchronous version here
+const templateOverview = fs.readFileSync("./templates/template-overview.html", 'utf-8')
+const templateCard = fs.readFileSync("./templates/template-card.html", 'utf-8')
+const templateProduct = fs.readFileSync("./templates/template-product.html", 'utf-8')
+
+const data = fs.readFileSync("./dev-data/data.json", 'utf-8')
+const dataObj = JSON.parse(data)
+
+//This runs every time a request is made
+const server = http.createServer((req, res) =>{
+    const {query, pathname} = url.parse(req.url, true)
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataObj.map(card => replaceTemplate(templateCard, card)).join('')
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
+    }
+    else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'})
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
+    }
+    else if (pathname === '/api') {
+        res.writeHead(200, {'content-type': 'application/json'})
+        res.end(data)
     }
     else {
         res.writeHead(404, {
